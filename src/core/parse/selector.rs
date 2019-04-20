bbc55c97dc265c891c4f10b119c3fe8f806d9bfd
RanolP <public.ranolp@gmail.com>
2019-04-20 16:24:48 +0900

    Update selector filtering code

--- src/core/parse/selector.rs
+++ src/core/parse/selector.rs
@@ -1,7 +1,8 @@
 use crate::core::node::{Selector, SelectorGroup, SelectorPart};
 use proc_macro::{Delimiter, Span, TokenStream, TokenTree};
-use std::iter::Peekable;
 
+use std::collections::HashMap;
+use std::iter::Peekable;
 pub fn parse_selector_group<I: 'static>(
     read_tokens: Vec<TokenTree>,
     tokens: &mut Peekable<I>,
@@ -41,56 +42,64 @@ where
     let mut ignore_token = false;
 
     let construct_selector = |selector_parts: Vec<SelectorPart>| {
+        let filters = {
+            let mut map = HashMap::<&str, Box<dyn Fn(&&SelectorPart) -> bool>>::with_capacity(2);
+            map.insert(
+                "pseudo class",
+                Box::new(|part| match part {
+                    SelectorPart::PseudoClass { .. } => true,
+                    _ => false,
+                }),
+            );
+            map.insert(
+                "pseudo element",
+                Box::new(|part| match part {
+                    SelectorPart::PseudoElement { .. } => true,
+                    _ => false,
+                }),
+            );
+            map.insert(
+                "element",
+                Box::new(|part| match part {
+                    SelectorPart::Element { .. } => true,
+                    _ => false,
+                }),
+            );
+            map
+        };
         if !selector_parts.is_empty() {
-            let filtered = selector_parts
-                .iter()
-                .filter(|part| {
-                    if let SelectorPart::PseudoClass { .. } = part {
-                        true
-                    } else {
-                        false
-                    }
-                })
-                .collect::<Vec<&SelectorPart>>();
+            let mut duplicate_detected = false;
+            for (name, filter) in filters {
+                let filtered = selector_parts
+                    .iter()
+                    .filter(filter)
+                    .collect::<Vec<&SelectorPart>>();
 
-            if filtered.len() >= 2 {
-                for part in filtered {
-                    if let Some(span) = part.span() {
-                        span.error("Use only one pseudo class").emit();
+                if filtered.len() >= 2 {
+                    for part in filtered {
+                        if let Some(span) = part.span() {
+                            span.error(format!("Use only one {}", name)).emit();
+                            duplicate_detected = true;
+                        }
                     }
                 }
             }
 
-            let filtered = selector_parts
-                .iter()
-                .filter(|part| {
-                    if let SelectorPart::PseudoElement { .. } = part {
-                        true
-                    } else {
-                        false
-                    }
+            if duplicate_detected {
+                None
+            } else {
+                Some(Selector {
+                    parts: selector_parts
+                        .into_iter()
+                        .rev()
+                        .skip_while(|part| *part == SelectorPart::Spacing)
+                        .map(|part| part.clone())
+                        .collect::<Vec<SelectorPart>>()
+                        .into_iter()
+                        .rev()
+                        .collect::<Vec<SelectorPart>>(),
                 })
-                .collect::<Vec<&SelectorPart>>();
-
-            if filtered.len() >= 2 {
-                for part in filtered {
-                    if let Some(span) = part.span() {
-                        span.error("Use only one pseudo element").emit();
-                    }
-                }
             }
-
-            Some(Selector {
-                parts: selector_parts
-                    .into_iter()
-                    .rev()
-                    .skip_while(|part| *part == SelectorPart::Spacing)
-                    .map(|part| part.clone())
-                    .collect::<Vec<SelectorPart>>()
-                    .into_iter()
-                    .rev()
-                    .collect::<Vec<SelectorPart>>(),
-            })
         } else {
             None
         }
@@ -132,11 +141,9 @@ where
             }
             _ => {
                 if let Some(result) = parse_selector_part(&current, tokens) {
-                    if let Some(last_part_span) = last_part_span {
-                        if let Some(span) = result.span() {
-                            if last_part_span.end() != span.start() {
-                                selector_parts.push(SelectorPart::Spacing);
-                            }
+                    if let (Some(last_part_span), Some(span)) = (last_part_span, result.span()) {
+                        if last_part_span.end() != span.start() {
+                            selector_parts.push(SelectorPart::Spacing);
                         }
                     }
                     last_part_span = result.span().or(last_part_span);
@@ -239,7 +246,8 @@ where
             let result = parse_identifier(None, tokens);
             if let Some((ident, span)) = result {
                 let span = current.span().join(span).expect("In the same file");
-                // todo: css namespace support (e.g. `svg|a`)
+                // todo: css namespace support (e.g. `svg|a`, `|a`, `*|a`)
+                // ? check required: should we filter identifier by html-element-set?
                 Some(SelectorPart::Element {
                     span: span,
                     namespace: None,
@@ -252,11 +260,7 @@ where
     }
     //? S = Q(CQ)*
     //? C = + | > | ~
-    //? Q = (t|u)? (a|p|n)*
-    //? t = T?e
-    //? T = (ident|'*')? '|'
-    //? e = all element name
-    //? u = T? '*'
+    //? Q = (a|p|n)*
     //? a = '[' T ident (('^=' | '$=' | '*=' | '=' | '~=' | '|=') (ident | String))? ']'
     //? p = ':'{1,2} ident ('('expr')')?
     //? n = ':not(' (t|u|h|c|a|p) ')'
