0c8d5de796c9772916cd75ae21458399e8db6f66
RanolP <public.ranolp@gmail.com>
2019-04-23 00:06:05 +0900

    Make comment more clear

--- src/css_use_impl.rs
+++ src/css_use_impl.rs
@@ -6,8 +6,7 @@ pub fn css_use(item: TokenStream) -> TokenStream {
     // todo: value inlining
     // println!("CSS USE\n{:?}\n", item);
 
-    // the variable maybe used by rustyle
-    // so should ignore the unused warning
+    // ? The variable maybe used by rustyle, so should ignore the unused warning
     let preamble_tokens = TokenStream::from(quote! {
       #[allow(unused)]
     });
